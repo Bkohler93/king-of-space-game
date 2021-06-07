09aa1760979e28971e88f6c476aa4b85c506d2f0
Brett Kohler <brettkohler93@gmail.com>
2021-06-07 14:10:07 -0700

    Added live updates to scoreboard

--- client/src/client.js
+++ client/src/client.js
@@ -1,130 +1,141 @@
-import {animate, drawScene} from './scene.js'
+import { animate, drawScene } from "./scene.js";
 
 //connect to server
 const sock = io();
 
 //Game variables
-const playerMe = {name: ""}
+const playerMe = { name: "" };
 
 //from server
-sock.on('message', logChat) //receive chat
-sock.on('playerEntered', logNewPlayer)  //receive new player
-sock.on('drawFullScene', drawScene) //draw scene with all players
-sock.on('info', registerId) 
+sock.on("message", logChat);
+sock.on("playerEntered", logNewPlayer);
+sock.on("drawFullScene", drawScene);
+sock.on("info", registerId);
+sock.on("updateScoreboard", updateScoreboard);
 
 //DOM elements
-const submitNameButton = document.getElementById('submit-name')
-const submitChatButton = document.getElementById('send-chat')
-const orangeButton = document.getElementById('submit-orange')
-const greenButton = document.getElementById('submit-green')
-const blueButton = document.getElementById('submit-blue')
-var canvas = document.getElementById('game-canvas')
-var color = 0
+const submitNameButton = document.getElementById("submit-name");
+const submitChatButton = document.getElementById("send-chat");
+const orangeButton = document.getElementById("submit-orange");
+const greenButton = document.getElementById("submit-green");
+const blueButton = document.getElementById("submit-blue");
+const scoreboard = document.getElementById("score-list");
+var canvas = document.getElementById("game-canvas");
+var color = 0;
 
 //listeners
-window.addEventListener('keydown', event => {
-    if (event.code === 'Space')
-        event.preventDefault()
-})
+window.addEventListener("keydown", (event) => {
+  if (event.code === "Space") event.preventDefault();
+});
 
-submitChatButton.addEventListener('click', submitChat)
-submitNameButton.addEventListener('click', sendPlayerDetails)
-orangeButton.addEventListener('click', setOrange)
-greenButton.addEventListener('click', setGreen)
-blueButton.addEventListener('click', setBlue)
+submitChatButton.addEventListener("click", submitChat);
+submitNameButton.addEventListener("click", sendPlayerDetails);
+orangeButton.addEventListener("click", setOrange);
+greenButton.addEventListener("click", setGreen);
+blueButton.addEventListener("click", setBlue);
 
 function setOrange() {
-   playerMe.color = "orange"
-   console.log("== playerMe.color:", playerMe.color)
-   orangeButton.style.backgroundColor = "orange"
-   greenButton.style.backgroundColor = "#efefef"
-   blueButton.style.backgroundColor = "#efefef"
+  playerMe.color = "orange";
+  console.log("== playerMe.color:", playerMe.color);
+  orangeButton.style.backgroundColor = "orange";
+  greenButton.style.backgroundColor = "#efefef";
+  blueButton.style.backgroundColor = "#efefef";
 }
 
 function setGreen() {
-   playerMe.color = "green"
-   console.log("== playerMe.color:", playerMe.color)
-   orangeButton.style.backgroundColor = "#efefef"
-   greenButton.style.backgroundColor = "green"
-   blueButton.style.backgroundColor = "#efefef"
+  playerMe.color = "green";
+  console.log("== playerMe.color:", playerMe.color);
+  orangeButton.style.backgroundColor = "#efefef";
+  greenButton.style.backgroundColor = "green";
+  blueButton.style.backgroundColor = "#efefef";
 }
 
 function setBlue() {
-   playerMe.color = "#3486eb"
-   console.log("== playerMe.color:", playerMe.color)
-   orangeButton.style.backgroundColor = "#efefef"
-   greenButton.style.backgroundColor = "#efefef"
-   blueButton.style.backgroundColor = "#3486eb"
+  playerMe.color = "#3486eb";
+  console.log("== playerMe.color:", playerMe.color);
+  orangeButton.style.backgroundColor = "#efefef";
+  greenButton.style.backgroundColor = "#efefef";
+  blueButton.style.backgroundColor = "#3486eb";
 }
 
 function sendPlayerDetails() {
+  var name = getSubmitNameText();
+  console.log("playerMe.color:", playerMe.color);
 
-    var name = getSubmitNameText();
-    console.log("playerMe.color:", playerMe.color)
-    
-    if (!name) {
-        alert('Please enter your name and color')
-        return
-    }
-    else if(playerMe.color != "orange" && playerMe.color != "green" && playerMe.color != "#3486eb") {
-       alert('Please enter your name and color')
-       return
-    }
-    playerMe.name = name;
-    playerMe.hit = false;
-    
-    //remove name input area
-    var enterGamePrompt = document.getElementById('enter-game')
-    var screenWrapper = document.querySelector('.screen-wrapper')
-    enterGamePrompt.style.display = 'none'
-    screenWrapper.style.backgroundColor = 'white'
+  if (!name) {
+    alert("Please enter your name and color");
+    return;
+  } else if (
+    playerMe.color != "orange" &&
+    playerMe.color != "green" &&
+    playerMe.color != "#3486eb"
+  ) {
+    alert("Please enter your name and color");
+    return;
+  }
+  playerMe.name = name;
+  playerMe.hit = false;
 
-    //submit player to server
-    sock.emit('playerDetails', playerMe);
-    animate(playerMe.id)
+  //remove name input area
+  var enterGamePrompt = document.getElementById("enter-game");
+  var screenWrapper = document.querySelector(".screen-wrapper");
+  enterGamePrompt.style.display = "none";
+  screenWrapper.style.backgroundColor = "white";
+
+  //submit player to server
+  sock.emit("playerDetails", playerMe);
+  animate(playerMe.id);
 }
 
 function getSubmitNameText() {
-    return document.getElementById('name').value;
+  return document.getElementById("name").value;
 }
 
 function submitChat() {
-    var chatInput = document.getElementById('chat')
-    if (!chatInput.value) return
+  var chatInput = document.getElementById("chat");
+  if (!chatInput.value) return;
 
-    var text = playerMe.name +  ': ' + chatInput.value
-    chatInput.value = ''
-    sock.emit('message', text)
+  var text = playerMe.name + ": " + chatInput.value;
+  chatInput.value = "";
+  sock.emit("message", text);
 }
 
-
 function logChat(text) {
-    
-    const chatList = document.getElementById('chats')
-    const newChat = document.createElement('li')
-    newChat.textContent = text
+  const chatList = document.getElementById("chats");
+  const newChat = document.createElement("li");
+  newChat.textContent = text;
 
-    chatList.appendChild(newChat)
-    var chatHeight = chatList.scrollHeight
-    
-    //show most recent chat
-    chatList.scrollTo(0, chatHeight)
+  chatList.appendChild(newChat);
+  var chatHeight = chatList.scrollHeight;
+
+  //show most recent chat
+  chatList.scrollTo(0, chatHeight);
 }
 
 function logNewPlayer(player) {
-
-    const newPlayerMessage = document.createElement('li')
-    const chatList = document.getElementById('chats')
-    newPlayerMessage.textContent = `${player.name} has connected.`
-    newPlayerMessage.style.color = String(player.color);
-    chatList.appendChild(newPlayerMessage)
-    chatList.scrollTop = parent.scrollHeight
+  const newPlayerMessage = document.createElement("li");
+  const chatList = document.getElementById("chats");
+  newPlayerMessage.textContent = `${player.name} has connected.`;
+  newPlayerMessage.style.color = String(player.color);
+  chatList.appendChild(newPlayerMessage);
+  chatList.scrollTop = parent.scrollHeight;
 }
 
 function registerId(userId) {
-    playerMe.id = userId
-    console.log(`user id is ${userId}`)
+  playerMe.id = userId;
+  console.log(`user id is ${userId}`);
 }
 
+function updateScoreboard(players) {
+  while (scoreboard.hasChildNodes()) {
+    scoreboard.removeChild(scoreboard.lastChild);
+  }
 
-export {sock, playerMe}
+  for (let player of players) {
+    var playerScore = document.createElement("li");
+    playerScore.textContent = player[0] + "     " + player[1];
+    scoreboard.appendChild(playerScore);
+  }
+}
+
+export { sock, playerMe };
--- server/server.js
+++ server/server.js
@@ -1,110 +1,121 @@
-const http = require('http')
-const express = require('express')
-const socketio = require('socket.io')
-const FPS = 30
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const FPS = 30;
 
 //create app
-const app = express()
+const app = express();
 
 //serve from client folder
-app.use(express.static(`${__dirname}/../client`))
+app.use(express.static(`${__dirname}/../client`));
 
 //add event listener for app
-const server = http.createServer(app)
-const io = socketio(server) //wrap around server. filter out requests related 
+const server = http.createServer(app);
+const io = socketio(server); //wrap around server. filter out requests related
 //                            to socketio, other request pass to express
 
 //players object
-var players = {}
-var deadPlayers ={}
-
+var players = {};
+var deadPlayers = {};
 
 //when new client is connected, this event listener runs and creates 'sock' object
-io.on('connection', (sock) => {
-    const userId = sock.id
-    players[userId] = {}
-    sock.emit('info', userId)
-    sock.emit('message', 'You are connected')
+io.on("connection", (sock) => {
+  const userId = sock.id;
+  players[userId] = {};
+  sock.emit("info", userId);
+  sock.emit("message", "You are connected");
 
-    //sock.emit sends to ONE client, io.emit sends to ALL clients
-    //sock.on receieves
-    sock.on('message', (text) => io.emit('message', text))
-    sock.on('playerDetails', (player) => processNewPlayer(player))
-    sock.on('submitPlayerDataAndLaser', (playerId, x, y, a, laserX, laserY) =>
-        addPlayerSceneWithLaser(playerId, x, y, a, laserX, laserY))
-    sock.on('playerHit', (playerId) => resetPlayerShip(playerId))
-    sock.on('shipDestroyed', (playerId) => addDestroyCount(playerId))
+  //sock.emit sends to ONE client, io.emit sends to ALL clients
+  //sock.on receieves
+  sock.on("message", (text) => io.emit("message", text));
+  sock.on("playerDetails", (player) => processNewPlayer(player));
+  sock.on("submitPlayerDataAndLaser", (playerId, x, y, a, laserX, laserY) =>
+    addPlayerSceneWithLaser(playerId, x, y, a, laserX, laserY)
+  );
+  sock.on("playerHit", (playerId) => resetPlayerShip(playerId));
+  sock.on("shipDestroyed", (playerId) => addDestroyCount(playerId));
 
-    setInterval(gameUpdate, 1000 / FPS)
+  setInterval(gameUpdate, 1000 / FPS);
+  setInterval(scoreUpdate, 3000); //updates every 3 seconds
 
-    sock.on('disconnect', () => {
-        console.log(`Player ${players[sock.id].name} has disconnected`)
-        delete players[sock.id]
-
-    })        
-})
+  sock.on("disconnect", () => {
+    console.log(`Player ${players[sock.id].name} has disconnected`);
+    delete players[sock.id];
+  });
+});
 
 //send all scene data, check if player hitTimer is over (respawns if it is)
 function gameUpdate() {
+  Object.keys(deadPlayers).forEach((id) => {
+    deadPlayers[id]--;
+    if (deadPlayers[id] < 30) {
+      players[id].hit = false;
+      delete deadPlayers[id];
+    }
+  });
 
-    Object.keys(deadPlayers).forEach(id => {
-        deadPlayers[id]--
-        if (deadPlayers[id] < 30) {
-            players[id].hit = false
-            delete deadPlayers[id]
-        }
-    })
-
-    io.emit('drawFullScene', players)
+  io.emit("drawFullScene", players);
 }
 
-//executes upon receiving hit from a socket with playername that is hit. 
+//update player scoreboard, emit out leaderboard
+function scoreUpdate() {
+  var playerScores = [];
+
+  for (let key in players) {
+    if (players[key].hasOwnProperty("name")) {
+      playerScores.push([players[key].name, players[key].shipsDestroyed]);
+    }
+  }
+
+  playerScores.sort((first, second) => second[1] - first[1]);
+
+  io.emit("updateScoreboard", playerScores);
+}
+
+//executes upon receiving hit from a socket with playername that is hit.
 // send result to hit player
 function resetPlayerShip(playerId) {
-
-    deadPlayers[playerId] = 30
-    players[playerId].hit = true
-    players[playerId].shipsDestroyed = 0
-    io.to(playerId).emit('spaceshipHit')
+  deadPlayers[playerId] = 30;
+  players[playerId].hit = true;
+  players[playerId].shipsDestroyed = 0;
+  io.to(playerId).emit("spaceshipHit");
 }
 
 //executes when player lands a shot on another player
 function addDestroyCount(playerId) {
-
-    players[playerId].shipsDestroyed++
+  players[playerId].shipsDestroyed++;
 }
 //populate each player object with lasers and new positions
 function addPlayerSceneWithLaser(playerId, x, y, a, laserX, laserY) {
-    players[playerId].x = x
-    players[playerId].y = y
-    players[playerId].a = a
-    players[playerId].ready = true
-    players[playerId].laserX = laserX
-    players[playerId].laserY = laserY
+  players[playerId].x = x;
+  players[playerId].y = y;
+  players[playerId].a = a;
+  players[playerId].ready = true;
+  players[playerId].laserX = laserX;
+  players[playerId].laserY = laserY;
 }
-
 
 //store player and laser object into arrays
 //send notification in chat to all players
 function processNewPlayer(player) {
-    players[player.id].name = player.name
-    players[player.id].hitCount = 0
-    players[player.id].shipsDestroyed = 0
-    players[player.id].destroyCount = 0
+  players[player.id].name = player.name;
+  players[player.id].hitCount = 0;
+  players[player.id].shipsDestroyed = 0;
+  players[player.id].destroyCount = 0;
 
-    io.emit('playerEntered', players[player.id])
+  io.emit("playerEntered", players[player.id]);
 }
 
 //check to be sure server is running
-server.on('error', (err) => {
-    console.error(err)
-})
+server.on("error", (err) => {
+  console.error(err);
+});
 
-//server needs to listen on port 
-port = process.env.PORT || 8022
-server.listen(port, function() {
-   console.log("Server is listening on port " + port)
-})
+//server needs to listen on port
+port = process.env.PORT || 8022;
+server.listen(port, function () {
+  console.log("Server is listening on port " + port);
+});
 
 /*server.listen(process.env.PORT || 8022, () =>{
     console.log("Server is listening on port 8022...")
