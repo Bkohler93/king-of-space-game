441fb0cfa0af9241efb18d4e7a6f6613b02c4f58
Brett Kohler <brettkohler93@gmail.com>
2021-06-08 16:43:30 -0700

    connected mongoDB

--- server/server.js
+++ server/server.js
@@ -1,22 +1,129 @@
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
-const FPS = 30;
+const { MongoClient } = require("mongodb");
 
-//create app
 const app = express();
-
-//serve from client folder
 app.use(express.static(`${__dirname}/../client`));
-
-//add event listener for app
 const server = http.createServer(app);
-const io = socketio(server); //wrap around server. filter out requests related
-//                            to socketio, other request pass to express
+const io = socketio(server); // socket.io wraps around server. filters out requests related
+//                            to socket.io, other request pass to express
+const uri =
+  "mongodb+srv://cs290kingofspace:kingofspace1234@kingofspace.o5a9z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
+const client = new MongoClient(uri);
+
+const FPS = 30;
 //players object
 var players = {};
 var deadPlayers = {};
+
+//use database
+async function main() {
+  //can someone store the uri value to the right as environment
+  //variable... couldnt get it to workhttps://king-of-space.herokuapp.com/
+  try {
+    await client.connect();
+
+    //DATABASE INQUIRIES HERE
+    // const leaderboard = await findOneListingByName(client);
+    // console.log(`== stored leaderboard data:`);
+    // console.log(leaderboard);
+
+    await updateLeaderboardExistingScore(client, ["Jenny", 10]);
+    await updateLeaderboardNewScore(client, ["brett", 12]);
+
+    await deleteLeaderboardScore(client, "brett");
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.close();
+  }
+}
+main().catch(console.error);
+
+//C(rud) -- most likely don't need to use this function
+async function createListing(client, newListing) {
+  const result = await client
+    .db("king-of-space")
+    .collection("leaderboard")
+    .insertOne(newListing);
+
+  console.log(`New listing created with id: ${result.insertedId}`);
+}
+
+//c(R)ud
+async function storeLeaders(client) {
+  const result = await client
+    .db("king-of-space")
+    .collection("leaderboard")
+    .findOne({ name: "leaderboard info" });
+
+  if (result) {
+    console.log("== Found 'leaders' in collection 'leaderboard'");
+  } else {
+    console.log(`== 'leaders' not found`);
+  }
+
+  return result.leaders;
+}
+
+//cr(U)d
+async function updateLeaderboardExistingScore(client, nameAndNewScoreArr) {
+  const result = await client
+    .db("king-of-space")
+    .collection("leaderboard")
+    .updateOne(
+      { name: "leaderboard info", "leaders.name": nameAndNewScoreArr[0] },
+      { $set: { "leaders.$.score": nameAndNewScoreArr[1] } }
+    );
+
+  console.log(`${result.matchedCount} document(s) matched query critiera`);
+  console.log(`${result.modifiedCount} documents was/were updated`);
+}
+
+//cru(D)
+async function deleteLeaderboardScore(client, name) {
+  const result = await client
+    .db("king-of-space")
+    .collection("leaderboard")
+    .updateOne(
+      { name: "leaderboard info" },
+      { $pull: { leaders: { name: name } } }
+    );
+
+  console.log(`${result.matchedCount} document(s) matched query critiera`);
+  console.log(`${result.modifiedCount} documents was/were updated`);
+}
+
+async function updateLeaderboardNewScore(client, nameAndScoreArr) {
+  const result = await client
+    .db("king-of-space")
+    .collection("leaderboard")
+    .updateOne(
+      {
+        name: "leaderboard info",
+      },
+      {
+        $push: {
+          leaders: { name: nameAndScoreArr[0], score: nameAndScoreArr[1] },
+        },
+      }
+    );
+
+  console.log(`${result.matchedCount} document(s) matched query critiera`);
+  console.log(`${result.modifiedCount} documents was/were updated`);
+}
+
+async function listDatabases(client) {
+  const databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => {
+    console.log(`- ${db.name}`);
+  });
+  findOneListingByName(client, "leaderboard");
+}
 
 //when new client is connected, this event listener runs and creates 'sock' object
 io.on("connection", (sock) => {
@@ -26,7 +133,7 @@ io.on("connection", (sock) => {
   sock.emit("message", "You are connected");
 
   //sock.emit sends to ONE client, io.emit sends to ALL clients
-  //sock.on receieves
+  //sock.on receieves from any client
   sock.on("message", (text) => io.emit("message", text));
   sock.on("playerDetails", (player) => processNewPlayer(player));
   sock.on("submitPlayerDataAndLaser", (playerId, x, y, a, laserX, laserY) =>
